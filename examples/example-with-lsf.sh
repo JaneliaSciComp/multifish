74100d386089bc34c0a08c6fb6c9b6ef22bd6b9c
Cristian Goina <goinac@janelia.hhmi.org>
2021-01-13 18:59:16 -0500

    use 2 rounds for parameters

--- examples/example-with-lsf.sh
+++ examples/example-with-lsf.sh
@@ -8,4 +8,5 @@
         --spark_work_dir "$PWD/local" \
         --stitching_app "external-modules/stitching-spark/target/stitching-spark-1.8.2-SNAPSHOT.jar" \
         --data_dir /nrs/scicompsoft/goinac/multifish/ex1 \
-        --acq_names LHA3_R3
+        --acq_names "LHA3_R3 LHA3_R5"
+
