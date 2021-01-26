29402ad009e55671a1402bf652054f545439ebd9
Cristian Goina <goinac@hhmi.org>
2021-01-26 16:51:16 -0500

    example with local singularity

---
+++ examples/example-with-localsingularity.sh
@@ -0,0 +1,15 @@
+./main.nf \
+        -with-tower 'http://nextflow.int.janelia.org/api' \
+        -profile localsingularity \
+        --runtime_opts "-B /nrs/scicompsoft/goinac" \
+        --workers 4 \
+        --worker_cores 4 \
+        --driver_memory 15g \
+        --spark_work_dir "$PWD/local" \
+        --stitching_app "$PWD/external-modules/stitching-spark/target/stitching-spark-1.8.2-SNAPSHOT.jar" \
+        --data_dir /nrs/scicompsoft/goinac/multifish/ex1 \
+        --stitching_output stitching \
+        --segmentation_model_dir "$PWD/models/starfinity-model" \
+        --reference_acq_name LHA3_R3 \
+        --acq_names "LHA3_R3"
+
