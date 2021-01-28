e6e17fe02bc78cd706f5df9c576a4ed6436bfbe2
Konrad Rokicki <rokickik@janelia.hhmi.org>
2021-01-28 17:10:50 -0500

    run on cluster, with tower

--- examples/run_registration.sh
+++ examples/run_registration.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#PROFILE="-profile localsingularity"
-#PROFILE='-profile lsf --lsf_opts "-P multifish"'
-#-with-tower 'http://nextflow.int.janelia.org/api' \
-
-#./workflows/registration.nf -profile lsf --lsf_opts "-P multifish" \
-./workflows/registration.nf -profile localsingularity \
+#./workflows/registration.nf -profile localsingularity \
+./workflows/registration.nf -profile lsf --lsf_opts "-P multifish" \
+    -with-tower 'http://nextflow.int.janelia.org/api' \
     --runtime_opts "-B /nrs/scicompsoft/goinac/multifish -B /nrs/scicompsoft/rokicki"  \
     --fixed=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R3/stitching_kr/export.n5 \
     --moving=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R5/stitching_kr/export.n5 \
