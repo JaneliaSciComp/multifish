0e6f1adfdcd919f7b46053e868f1e66a4ca9bd6c
Konrad Rokicki <rokickik@janelia.hhmi.org>
2021-01-29 00:12:10 -0500

    next dir

--- examples/run_registration.sh
+++ examples/run_registration.sh
@@ -6,5 +6,5 @@
     --runtime_opts "-B /nrs/scicompsoft/goinac/multifish -B /nrs/scicompsoft/rokicki"  \
     --fixed=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R3/stitching_kr/export.n5 \
     --moving=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R5/stitching_kr/export.n5 \
-    --outdir=/nrs/scicompsoft/rokicki/multifish/LHA_R5_TO_R3_lsf4 $@
+    --outdir=/nrs/scicompsoft/rokicki/multifish/LHA_R5_TO_R3_lsf5 $@
 
