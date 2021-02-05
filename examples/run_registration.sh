5c9a56ee88afb819d880c2ef92ba4c809100524a
Konrad Rokicki <rokickik@janelia.hhmi.org>
2021-02-05 11:45:00 -0500

    updated paths

--- examples/run_registration.sh
+++ examples/run_registration.sh
@@ -4,7 +4,7 @@
 ./workflows/registration.nf -profile lsf --lsf_opts "-P multifish" \
     -with-tower 'http://nextflow.int.janelia.org/api' \
     --runtime_opts "-B /nrs/scicompsoft/goinac/multifish -B /nrs/scicompsoft/rokicki"  \
-    --fixed=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R3/stitching_kr/export.n5 \
-    --moving=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R5/stitching_kr/export.n5 \
-    --reg_outdir=/nrs/scicompsoft/rokicki/multifish/LHA_R5_TO_R3_lsf $@
+    --fixed=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R3_subset/stitching/export.n5 \
+    --moving=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R5_subset/stitching/export.n5 \
+    --reg_outdir=/nrs/scicompsoft/rokicki/multifish/LHA_R5_TO_R3_subset $@
 
