9455911d797afaeeba8f8a9f8da1bf6fe27d595c
Konrad Rokicki <rokickik@janelia.hhmi.org>
2021-02-10 10:49:19 -0500

    updated to use new main

--- examples/run_registration.sh
+++ examples/run_registration.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#./workflows/registration.nf -profile localsingularity \
-./workflows/registration.nf -profile lsf --lsf_opts "-P multifish" \
+DIR=$(cd "$(dirname "$0")"; pwd)
+#$DIR/../main-registration.nf -profile lsf --lsf_opts "-P multifish" \
+$DIR/../main-registration.nf -profile localsingularity \
     -with-tower 'http://nextflow.int.janelia.org/api' \
     --runtime_opts "-B /nrs/scicompsoft/goinac/multifish -B /nrs/scicompsoft/rokicki"  \
     --fixed=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R3_subset/stitching/export.n5 \
     --moving=/nrs/scicompsoft/goinac/multifish/ex1/LHA3_R5_subset/stitching/export.n5 \
-    --reg_outdir=/nrs/scicompsoft/rokicki/multifish/LHA_R5_TO_R3_subset $@
+    --outdir=/nrs/scicompsoft/rokicki/multifish/LHA_R5_TO_R3_subset2 $@
 
