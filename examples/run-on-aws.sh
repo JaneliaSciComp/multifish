6ba50dfe0cd2d62623f2ed53a91b01789f51cf13
Cristian Goina <goinac@hhmi.org>
2021-05-06 14:50:02 -0400

    update aws example

--- examples/run-on-aws.sh
+++ examples/run-on-aws.sh
@@ -4,15 +4,25 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 
 nextflow run $DIR/../main.nf \
     -profile awsbatch \
-    -e.SPARK_LOCAL_DIR=/scratch/multifish/spark \
     -w s3://janelia-nextflow-demo/multifish/work \
-    --spark_container_repo "job-definition://easi-multifish-dev-spark-job-definition" \
+    --skip "stitching,registration,segmentation,spot_extraction" \
     --workers 1 \
     --worker_cores 16 \
-    --gb_per_core 1 \
-    --spark_work_dir "/scratch/multifish/work" \
-    --data_dir "/scratch/multifish/small" \
-    --output_dir "/scratch/multifish/results/small" \
+    --wait_for_spark_timeout_seconds 3600 \
+    --sleep_between_timeout_checks_seconds 2 \
+    --gb_per_core 4 \
+    --channels "c0,c1" \
+    --stitching_ref 1 \
+    --dapi_channel c1 \
+    --segmentation_cpus 1 \
+    --spot_extraction_xy_stride 512 \
+    --spot_extraction_z_stride 256 \
+    --spot_extraction_cpus 2 \
+    --spot_extraction_memory 8 \
+    --spark_local_dir "//tmp" \
+    --spark_work_dir "/efs-multifish/spark" \
+    --data_dir "/s3-multifish/small" \
+    --output_dir "/efs-multifish/results/small" \
     --acq_names "LHA3_R3_small,LHA3_R5_small" \
     --ref_acq "LHA3_R3_small" \
-    --segmentation_model_dir "/scratch/multifish/models/starfinity-model"
+    --segmentation_model_dir "/s3-multifish/models/starfinity-model"
