c62e5bd684b4b58e15f16b4e9f1d58a369522256
Konrad Rokicki <rokickik@janelia.hhmi.org>
2022-02-04 09:12:43 -0500

    removed segmentation download

--- setup.sh
+++ setup.sh
@@ -6,17 +6,8 @@ NEXTFLOW_SPARK_GITURL=https://github.com/JaneliaSciComp/nextflow-spark
 NEXTFLOW_SPARK_VERSION=1.4.0
 
 # Download Nextflow Spark module
+mkdir -p ${BASEDIR}/external-modules/spark
 curl -skL ${NEXTFLOW_SPARK_GITURL}/archive/${NEXTFLOW_SPARK_VERSION}.tar.gz \
     | tar -xz --strip-components=1 -C ${BASEDIR}/external-modules/spark
-
-# Download Starfinity model
-segmentation_dir=$BASEDIR/external-modules/segmentation
-segmentation_modeldir="$segmentation_dir/model/starfinity"
-if [ ! -e $segmentation_modeldir ]; then
-    echo "Extracting Starfinity model..."
-    mkdir -p segmentation_dir
-    $BASEDIR/bin/download_dataset.sh "$BASEDIR/data-sets/segmentation.txt" "$segmentation_dir"
-    unzip -o $segmentation_dir/model.zip -d $segmentation_dir/    
-fi
 
 echo "Setup complete"
