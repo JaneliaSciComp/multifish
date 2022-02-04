380d2c453e55cbcbcea44eeb99cfe8434630562c
Konrad Rokicki <rokickik@janelia.hhmi.org>
2022-02-04 09:08:14 -0500

    fixed path to download-dataset

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ segmentation_modeldir="$segmentation_dir/model/starfinity"
 if [ ! -e $segmentation_modeldir ]; then
     echo "Extracting Starfinity model..."
     mkdir -p segmentation_dir
-    $BASEDIR/data-sets/download_dataset.sh "$BASEDIR/data-sets/segmentation.txt" "$segmentation_dir"
+    $BASEDIR/bin/download_dataset.sh "$BASEDIR/data-sets/segmentation.txt" "$segmentation_dir"
     unzip -o $segmentation_dir/model.zip -d $segmentation_dir/    
 fi
 
